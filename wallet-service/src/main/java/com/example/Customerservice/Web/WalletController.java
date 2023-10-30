bceeaae460856ff20e93af6b1b2043bdd11f67bf
Soukainatabat <stabat292001@gmail.com>
2023-10-30 09:20:49 +0100

    v5

--- wallet-service/src/main/java/com/example/Customerservice/Web/WalletController.java
+++ wallet-service/src/main/java/com/example/Customerservice/Web/WalletController.java
@@ -1,7 +1,11 @@
 package com.example.Customerservice.Web;
 
+import com.example.Customerservice.Entities.Wallet;
 import com.example.Customerservice.Repositories.Walletrepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class WalletController {
@@ -9,6 +13,10 @@ public class WalletController {
 
     public WalletController(Walletrepository walletrepository) {
         this.walletrepository = walletrepository;
+    }
+    @GetMapping("/walletAll")
+    public List<Wallet> walletList(){
+        return walletrepository.findAll();
     }
 
 }
---
+++ wallet-service/src/main/java/com/example/Customerservice/walletserviceApplication.java
@@ -0,0 +1,14 @@
+package com.example.Customerservice;
+
+import com.example.Customerservice.Repositories.Walletrepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class walletserviceApplication {
+    public static void main(String[] args){
+
+    }
+
+
+}
